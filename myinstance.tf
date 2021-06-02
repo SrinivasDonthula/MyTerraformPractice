880d87060f2fed15aa1f1e1c29f705b722775555
SrinivasDonthula <48355692+SrinivasDonthula@users.noreply.github.com>
2021-06-02 20:42:55 +0530

    Update myinstance.tf

--- myinstance.tf
+++ myinstance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_web_instance" {
-    ami = "${lookup(var.images,var.region}"
+    ami = "${lookup(var.images,var.region)}"
     instance_type = "t2.micro"
     key_name = "SingaPore2KeyPair.pem"
     vpc_security_group_ids = ["${aws_security_group.web_security_group.id}"]
@@ -10,15 +10,26 @@ resource "aws_instance" "my_web_instance" {
     volume_tags = {
         Name = "my_web_instance_volume"
     }
-
+    
 provisioner "remote-exec" {
     inline = [
 "sudo mkdir -p /var/www/html/",
 "sudo yum update -y",
 "sudo yum install -y httpd",
 "sudo service httpd start",
-"sudo usermod -a -G apache ec2-user",
-"sudo chown -R ec2-user:apache /var/www",
+"sudo usermod -a -G apache ubuntu",
+"sudo chown -R ubuntu:apache /var/www",
 "sudo yum install -y mysql php php-mysql"
     ]
+}
+provisioner "file" {
+    source = "index.php"
+    destination = "/var/www/html/index.php"
+}
+connection {
+    type = "ssh"
+    user = "ubuntu"
+    password = ""
+    private_key = "${file("/root/MyTerraformPractice/SingaPore2KeyPair.pem")}"
+ }
 }
